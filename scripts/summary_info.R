39de8e9e5273b203eff1201d25743ce834ba4bdb
dsin <uxdsin@gmail.com>
2019-11-17 14:01:50 -0800

    Updated Summary Info

--- scripts/summary_info.R
+++ scripts/summary_info.R
@@ -1,3 +1,28 @@
-# A function that takes in a dataset and returns a list of info about it:
+library(dplyr)
+library(tidyr)
 
-five <- 5
+# Summary INFO
+# Returns a list containing
+# Number of Songs
+# Types of Genres
+# All Artists
+# Lowest Popularity Needed
+# Average Song Length (Seconds)
+
+get_summary_info <- function(df)  {
+  info <- list()
+  info$num_of_songs <- nrow(df)
+  info$genres <- df %>%
+    pull(Genre)
+  info$genres <- info$genres[!duplicated(info$genres)]
+  info$artists <- df %>%
+    pull(Artist.Name)
+  info$artists <- info$artists[!duplicated(info$artists)]
+  info$min_popularity <- df %>%
+    filter(Popularity == min(Popularity)) %>%
+    pull(Popularity)
+  info$average_length <- df %>%
+    summarise(avg = mean(Length., na.rm = TRUE)) %>%
+    pull(avg)
+  info
+}
