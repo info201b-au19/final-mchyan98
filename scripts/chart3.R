3d21dfe25c4b801d17664e931b3369efcb596c9a
unknown <mchyan98@uw.edu>
2019-11-17 16:05:42 -0800

    added chart 3

--- scripts/chart3.R
+++ scripts/chart3.R
@@ -1,3 +1,20 @@
 # chart 3
+# Dataset Top 50 Spotify Song - 2019
 
-three <- 3
+# question: which genre of music do people like best?
+# 
+
+library(ggplot2)
+library("dplyr") 
+
+popularity_off_bpm <- function(df){
+  ggplot(df) +
+  geom_smooth(mapping = aes(x = df$Beats.Per.Minute, y = df$Popularity))+
+  geom_point(mapping = aes(x = df$Beats.Per.Minute, y = df$Popularity), color = "Red") +
+  
+  labs(
+    title = "Popularity in relation to Beats per Minute",
+    x = "Beats per Minute",
+    y = "Popularity"
+  )
+}
